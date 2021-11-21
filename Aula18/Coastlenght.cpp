d77e2f0fa0cc73f6c8d6e5404d91107656c830d7
luismoroco <lmorocoramos@gmail.com>
2021-11-21 02:19:40 -0500

    GAA

---
+++ Aula18/Coastlenght.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<vector<int>> vii;
+
+// https://open.kattis.com/problems/coast
+
+int main(int argc, char const *argv[])
+{
+    int N, M;
+    cin >> N >> M;
+    vii x;
+        
+    
+    return 0;
+}
