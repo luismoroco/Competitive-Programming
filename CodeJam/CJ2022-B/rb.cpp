7ba63ebe8436afe19a6eacbb675b2c40da1befdf
luismoroco} <lmorocoramos@gmail.com>
2022-10-13 12:23:08 -0500

    CJ22B

---
+++ CodeJam/CJ2022-B/rb.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void run() {
+  int n, d, c ,m;
+  cin >> n >> d >> c >> m;
+
+  char str[n];
+  cin >> str;
+
+  
+
+
+  
+}
+
+int main(int, char**) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int T;
+  scanf("%d", &T);
+  for (int i = 1; i <= T; ++i) {
+    cout << "Case #" << i << ": "; run();
+  }
+
+  return 0;
+}
